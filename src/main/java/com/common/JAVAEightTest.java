92289cbb870f58e09ae155b0aec69ddabce81112
meihuai.liang <meihuai.liang@tendcloud.com>
2017-12-14 16:51:15 +0800

    first commit

--- src/main/java/com/common/JAVAEightTest.java
+++ src/main/java/com/common/JAVAEightTest.java
@@ -9,16 +9,20 @@ import java.util.stream.Collectors;
 public class JAVAEightTest {
     public static void main(String[] args) {
         List<Map<String,Object>> list =  new ArrayList<>();
-        Map<String,Object> map = new HashMap<>();
-        map.put("id",1);
-        Map<String,Object> map1 = new HashMap<>();
-        map1.put("id",2);
-        Map<String,Object> map2 = new HashMap<>();
-        map2.put("id",3);
-        list.add(map);
-        list.add(map1);
-        list.add(map2);
-        List<Integer> list1 = list.stream().map(f -> (Integer) map.get("id")+(Integer)f.get("id")).collect(Collectors.toList());
-        System.out.println(list1);
+        list.stream().forEach(m ->{
+            System.out.println("xxx");
+        });
+
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("id",1);
+//        Map<String,Object> map1 = new HashMap<>();
+//        map1.put("id",2);
+//        Map<String,Object> map2 = new HashMap<>();
+//        map2.put("id",3);
+//        list.add(map);
+//        list.add(map1);
+//        list.add(map2);
+//        List<Integer> list1 = list.stream().map(f -> (Integer) map.get("id")+(Integer)f.get("id")).collect(Collectors.toList());
+//        System.out.println(list1);
     }
 }
