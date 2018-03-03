c9150f0584b2d38253bd6b92404b2cce0fb47403
meihuai.liang <meihuai.liang@tendcloud.com>
2018-03-03 17:59:05 +0800

    deep jvm chapter ten 10-8.txt source code

---
+++ src/main/java/com/deep/jvm/chapterten/EaqualsTest.java
@@ -0,0 +1,24 @@
+package com.deep.jvm.chapterten;
+
+/**
+ * Created by tend on 2018/3/3.
+ */
+public class EaqualsTest {
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c == d); // true
+        System.out.println(e == f); // false
+        System.out.println(c == (a + b)); // true
+        System.out.println(c.equals(a + b)); // true
+        System.out.println(g == (a + b)); // true ??// why 向上转型??
+        System.out.println(g.equals(a + b)); // false
+    }
+
+
+}
